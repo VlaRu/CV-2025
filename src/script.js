24dca37ad3589b19c7c7d671c26838fc1cff4a9a
VlaRu <dn210690nvs@gmail.com>
2024-10-19 13:16:17 +0200

    refactor: change code

--- src/script.js
+++ src/script.js
@@ -1,34 +1,26 @@
-import {navigation} from "./modules/navigation.js";
+import {navigation, burgerNav, navigationElements } from "./modules/navigation.js";
 import {moveRightSlide, moveLeftSlide} from "./modules/sliderProject.js";
+import {burgerMenu, toggleBurgerMenu} from "./modules/burger.js";
+
 const prevButton = document.querySelector(".arrow-left");
 const nextButton = document.querySelector(".arrow-right");
-import {navigationElements} from "./modules/navigation.js"
-let isNavigationActive = false;
 
-function handleResize() {
+
+function handleResize(){
   if (window.innerWidth > 768) {
     navigationElements.forEach(section => {
       section.classList.add('none');
     });
-    navigationElements[0].classList.remove('none');
-    isNavigationActive = true;
-  } else {
+  }else{
     navigationElements.forEach(section => {
       section.classList.remove('none');
     });
-    if (isNavigationActive) {
-      isNavigationActive = false;
-    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (!isNavigationActive) {
-    navigation();
-    isNavigationActive = true;
-  }
-  handleResize();
-
+  navigation();
+  burgerNav()
   window.addEventListener('resize', handleResize);
 
   nextButton.addEventListener('click', () => {
@@ -39,3 +31,5 @@ document.addEventListener('DOMContentLoaded', () => {
     moveLeftSlide();
   });
 });
+
+burgerMenu.addEventListener('click', toggleBurgerMenu);
