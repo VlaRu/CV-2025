e1693ba4e0932627aa8d51d56d4d6a13487649f1
VlaRu <dn210690nvs@gmail.com>
2024-12-18 00:48:09 +0100

    refactor: add animation to elements

--- src/modules/navigation.js
+++ src/modules/navigation.js
@@ -1,31 +1,55 @@
-export const navigationElements = document.querySelectorAll('.section');
-export const burgerNavigation =document.querySelectorAll('.burger-list_navigation-element');
-const navigationList = document.querySelectorAll('.list_navigation-element');
+export const sectionNavigationElements = document.querySelectorAll(".section");
+export const burgerNavigation = document.querySelectorAll(
+  ".burger-list_navigation-element"
+);
+const navigationList = document.querySelectorAll(".list_navigation-element");
+export let currentSection = 0;
+
 
 export function hideSections() {
-  navigationElements.forEach(section => {
-    section.classList.add('none');
+  sectionNavigationElements.forEach((section) => {
+    section.classList.add("none");
   });
 }
 
 export function navigation() {
   navigationList.forEach((tab, i) => {
-    tab.addEventListener('click', () => {
+    tab.addEventListener("click", () => {
       hideSections();
-      navigationElements[i].classList.remove('none');
-      navigationList.forEach(tab => {
-        tab.classList.remove('bg');
+      sectionNavigationElements[i].classList.remove("none");
+      currentSection = i;
+      if (sectionNavigationElements[i].classList.contains('section-language') || sectionNavigationElements[i].classList.contains('section-skills')){
+        startAnimationRange()
+      }
+      navigationList.forEach((tab) => {
+        tab.classList.remove("bg");
       });
 
-      tab.classList.add('bg');
+      tab.classList.add("bg");
     });
   });
 }
 
 export function burgerNav() {
   burgerNavigation.forEach((el, i) => {
-    el.addEventListener('click', () => {
-      navigationElements[i].classList.remove('none');
-    })
-  })
+    el.addEventListener("click", () => {
+      sectionNavigationElements[i].classList.remove("none");
+      currentSection = i;
+    });
+  });
+}
+
+export function startAnimationRange(){
+  const rangeInputs = document.querySelectorAll(".input-range");
+
+  rangeInputs.forEach((input) => {
+    const value = input.value;
+    const max = input.max;
+    input.style.backgroundSize = `0% 100%`;
+
+    requestAnimationFrame(() => {
+      const backgroundSize = (value / max) * 100;
+      input.style.backgroundSize = `${backgroundSize}% 100%`;
+    });
+  });
 }
--- src/modules/sliderProject.js
+++ src/modules/sliderProject.js
@@ -1,17 +1,48 @@
 const carouselSlide = document.querySelector(".project_card-wrapper");
-
-export function handleSlider() {
-  console.log('Slider handled');
-}
+const sliderRange = document.getElementById("range-slider");
+let currentSlide = 0;
+const totalSlides = 6;
 
 export function moveRightSlide() {
   const lastImage = carouselSlide.lastElementChild;
   carouselSlide.removeChild(lastImage);
   carouselSlide.insertBefore(lastImage, carouselSlide.firstChild);
+  currentSlide = (currentSlide + 1) % (totalSlides + 1);
+  updateSliderRange();
 }
 
 export function moveLeftSlide() {
   const firstImage = carouselSlide.firstElementChild;
   carouselSlide.removeChild(firstImage);
   carouselSlide.appendChild(firstImage);
+  currentSlide = (currentSlide - 1 + totalSlides + 1) % (totalSlides + 1);
+  updateSliderRange();
 }
+
+export function rangeSlide() {
+  sliderRange.addEventListener("input", (e) => {
+    const value = parseInt(e.target.value);
+
+    if (value > currentSlide) {
+      for (let i = currentSlide; i < value; i++) {
+        moveLeftSlide();
+      }
+    } else if (value < currentSlide) {
+      for (let i = currentSlide; i > value; i--) {
+        moveRightSlide();
+      }
+    }
+    const backgroundSize = ((value / totalSlides) * 100).toFixed(2);
+    sliderRange.style.backgroundSize = `${backgroundSize}% 100%`;
+
+    currentSlide = value;
+  });
+}
+
+function updateSliderRange() {
+  sliderRange.value = currentSlide;
+  const backgroundSize = ((currentSlide / totalSlides) * 100).toFixed(2);
+  sliderRange.style.backgroundSize = `${backgroundSize}% 100%`;
+}
+
+rangeSlide()
