fde0eda705c7e2898ec6b17fd7782a14743c5695
vlaru <dn210690nvs@gmail.com>
2025-06-03 01:54:35 +0300

    feat: add projects data module

---
+++ src/modules/dataProject.js
@@ -0,0 +1,99 @@
+const projectsData = [
+  {
+    id: 1,
+    title: 'E-Commerce Platform',
+    description: 'A full-featured e-commerce platform with shopping cart and payment integration.',
+    projectImage: 'assets/images/commerce.jpg',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-react-4-1175110.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-typescript-3629120-3030260.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-webpack-2-1174981.png?f=webp&w=256',
+      'https://img.stackshare.io/service/7035/default_66f265943abed56bcdbfca1c866a4261b1fbb063.jpg',
+      'https://img.stackshare.io/service/3337/Q4L7Jncy.jpg'
+    ],
+    sourceLink: 'https://github.com/RS-Tech-Artisans/eCommerce-React',
+    liveLink: 'https://vladislavak.github.io/E-Commerce/'
+  },
+  {
+    id: 2,
+    title: 'Pokemon Game',
+    description: 'An interactive Pokemon game with character selection and battle mechanics.',
+    projectImage: 'assets/images/pokemon-scr.png',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-html5-40-1175193.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-css3-11-1175239.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp&w=256'
+    ],
+    sourceLink: 'https://github.com/VladislavaK/Pokemon-Game',
+    liveLink: 'https://vladislavak.github.io/Pokemon-Game/'
+  },
+  {
+    id: 3,
+    title: 'Weather App',
+    description: 'Real-time weather application with location-based forecasts.',
+    projectImage: 'assets/images/weather.jpg',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-html5-40-1175193.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-css3-11-1175239.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-api-1-1175127.png?f=webp&w=256'
+    ],
+    sourceLink: 'https://github.com/VladislavaK/Weather-App',
+    liveLink: 'https://vladislavak.github.io/Weather-App/'
+  },
+  {
+    id: 4,
+    title: 'Todo List',
+    description: 'A task management application with drag-and-drop functionality.',
+    projectImage: 'assets/images/todo-scr.png',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-html5-40-1175193.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-css3-11-1175239.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp&w=256'
+    ],
+    sourceLink: 'https://github.com/VladislavaK/Todo-List',
+    liveLink: 'https://vladislavak.github.io/Todo-List/'
+  },
+  {
+    id: 5,
+    title: 'Travel Blog',
+    description: 'A responsive travel blog with image galleries and location mapping.',
+    projectImage: 'assets/images/travel.png',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-html5-40-1175193.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-css3-11-1175239.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-react-4-1175110.png?f=webp&w=256'
+    ],
+    sourceLink: 'https://github.com/VladislavaK/Travel-Blog',
+    liveLink: 'https://vladislavak.github.io/Travel-Blog/'
+  },
+  {
+    id: 6,
+    title: 'Coffee House',
+    description: 'A coffee shop website with menu and online ordering system.',
+    projectImage: 'assets/images/coffee-house.jpg',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-html5-40-1175193.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-css3-11-1175239.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp&w=256'
+    ],
+    sourceLink: 'https://github.com/VladislavaK/Coffee-House',
+    liveLink: 'https://vladislavak.github.io/Coffee-House/'
+  },
+  {
+    id: 7,
+    title: 'SMM Landing Page',
+    description: 'A landing page for social media marketing services.',
+    projectImage: 'assets/images/smm-landing.png',
+    technologies: [
+      'https://cdn.iconscout.com/icon/free/png-256/free-html5-40-1175193.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-css3-11-1175239.png?f=webp&w=256',
+      'https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp&w=256'
+    ],
+    sourceLink: 'https://github.com/VladislavaK/SMM-Landing',
+    liveLink: 'https://vladislavak.github.io/SMM-Landing/'
+  }
+];
+
+export default projectsData; 
