334e053fe8a6b011fea0e44f8a5bb00c675ec46c
vlaru <dn210690nvs@gmail.com>
2025-06-03 01:49:53 +0300

    chore: add copy-webpack-plugin for asset management and update Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -11,6 +12,7 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'dist'),
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       clean: true,
+      assetModuleFilename: 'assets/[path][name][ext]'
     },
     module: {
       rules: [
@@ -25,6 +27,9 @@ module.exports = (env, argv) => {
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
+          generator: {
+            filename: 'assets/[path][name][ext]'
+          }
         },
       ],
     },
@@ -35,17 +40,30 @@ module.exports = (env, argv) => {
       new MiniCssExtractPlugin({
         filename: isProduction ? '[name].[contenthash].css' : '[name].css',
       }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/assets',
+            to: 'assets',
+            globOptions: {
+              ignore: ['**/*.DS_Store'],
+            },
+          }
+        ]
+      })
     ],
     devServer: {
       static: {
         directory: path.join(__dirname, 'dist'),
       },
       compress: true,
-      port: 9000,
+      port: 3000,
       hot: true,
+      open: true,
+      historyApiFallback: true
     },
     optimization: {
       runtimeChunk: 'single',
     },
   };
-}; 
+};
