7faa659dace5d68f0f8ec1641877aecd225d387d
vlaru <dn210690nvs@gmail.com>
2025-06-03 23:17:57 +0300

    fix: update image paths in HTML and CSS, enhance project section responsiveness, and implement touch support for slider functionality

--- src/modules/sliderProject.js
+++ src/modules/sliderProject.js
@@ -2,6 +2,9 @@ const carouselSlide = document.querySelector(".project_card-wrapper");
 const sliderRange = document.getElementById("range-slider");
 let currentSlide = 0;
 const totalSlides = 6;
+let touchStartX = 0;
+let touchEndX = 0;
+const SWIPE_THRESHOLD = 50;
 
 export function moveRightSlide() {
   const lastImage = carouselSlide.lastElementChild;
@@ -26,7 +29,6 @@ function updateSliderRange() {
 }
 
 function updateSlider(e){
-
   const rect = sliderRange.getBoundingClientRect();
   const offsetX = e.clientX - rect.left;
   const value = Math.round((offsetX / rect.width) * (sliderRange.max - sliderRange.min)) + parseInt(sliderRange.min);
@@ -46,8 +48,32 @@ function updateSlider(e){
   currentSlide = value;
 }
 
+function handleTouchStart(e) {
+  touchStartX = e.touches[0].clientX;
+}
+
+function handleTouchMove(e) {
+  touchEndX = e.touches[0].clientX;
+}
+
+function handleTouchEnd() {
+  const swipeDistance = touchEndX - touchStartX;
+ 
+  if (Math.abs(swipeDistance) > SWIPE_THRESHOLD) {
+    if (swipeDistance > 0) {
+      moveRightSlide();
+    } else {
+      moveLeftSlide();
+    }
+  }
+}
+
 export function rangeSlide() {
-  if (document.documentElement.clientWidth < 950) {
+  if (document.documentElement.clientWidth < 650) {
+    carouselSlide.addEventListener('touchstart', handleTouchStart, { passive: true });
+    carouselSlide.addEventListener('touchmove', handleTouchMove, { passive: true });
+    carouselSlide.addEventListener('touchend', handleTouchEnd, { passive: true });
+
     sliderRange.addEventListener("touchmove", (e) => {
       const touch = e.touches[0];
       updateSlider(touch);
@@ -64,4 +90,9 @@ rangeSlide()
 window.addEventListener("resize", () => {
   sliderRange.removeEventListener("mousemove", updateSlider);
   sliderRange.removeEventListener("touchmove", updateSlider);
+  carouselSlide.removeEventListener('touchstart', handleTouchStart);
+  carouselSlide.removeEventListener('touchmove', handleTouchMove);
+  carouselSlide.removeEventListener('touchend', handleTouchEnd);
+  rangeSlide();
 });
+
